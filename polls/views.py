82df26e7a748e523077b7ce5c1267c1b46195395
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-05-31 17:02:12 +0100

    working on views tutorial section

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,16 @@
+from unittest import loader
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hi, you're are the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(context, request)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s" % question_id)
