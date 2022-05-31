0f8ca3d7d96336ca864deb812f0327a4fb924266
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-05-31 17:26:15 +0100

    more views amendments

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(context, request)
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s" % question_id)
